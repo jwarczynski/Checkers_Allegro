066625379e0a9af0807405cfcd9c7138791a6d7d
Jędrzej Warczyński <jedrzej.warczynski@student.put.poznan.pl>
2023-07-18 21:03:39 +0200

    add pawn capture tests

---
+++ Test/pawnCaptureTest.h
@@ -0,0 +1,12 @@
+//
+// Created by Jj on 16/07/2023.
+//
+
+#ifndef CHECKERS_PAWNCAPTURETEST_H
+#define CHECKERS_PAWNCAPTURETEST_H
+
+void shouldYield2PawnCaptureMovesWith2CapturesEach();
+void shouldYield4PawnCaptureMovesWith2CapturesEach();
+
+
+#endif //CHECKERS_PAWNCAPTURETEST_H
