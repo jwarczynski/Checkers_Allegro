3b4e3a4046b344ebfe210d842a8e51514a566ac9
Jędrzej Warczyński <jedrzej.warczynski@student.put.poznan.pl>
2023-07-24 00:14:00 +0200

    add new tests

--- Test/testMain.c
+++ Test/testMain.c
@@ -3,15 +3,13 @@
 //
 
 #include "Unity/src/unity.h"
-#include "boardSetup.h"
 #include "gameEngine/playerMove.h"
 
-#include "queenCaptureTest.h"
-#include "pawnCaptureTest.h"
-
-extern Player currentPlayer;
-extern PlayerMoves playerMoves;
-extern Piece board[BOARD_SIZE][BOARD_SIZE];
+#include "include/queenCaptureTest.h"
+#include "include/pawnCaptureTest.h"
+#include "include/pawnMoveTest.h"
+#include "include/queenMoveTest.h"
+#include "include/gameEngineTest.h"
 
 
 void setUp() {}
@@ -21,9 +19,37 @@ void tearDown() {
 
 int main() {
     UNITY_BEGIN();
+
+
+    // QUEEN CAPTURE TESTS
     RUN_TEST(shouldYield2QueenCaptureMovesWith2CapturesEach);
     RUN_TEST(shouldYield3QueenCaptureMovesWith1Capture);
+
+    // PAWN CAPTURE TESTS
     RUN_TEST(shouldYield2PawnCaptureMovesWith2CapturesEach);
     RUN_TEST(shouldYield4PawnCaptureMovesWith2CapturesEach);
+
+    // PAWN MOVE TESTS
+    RUN_TEST(shouldYield2WhitePawnMoves);
+    RUN_TEST(shouldYield1WhitePawnMove);
+    RUN_TEST(shouldYield2BlackPawnMoves);
+    RUN_TEST(shouldYield1BlackPawnMove);
+    RUN_TEST(shouldYield2WhitePawnsWith2MovesEach);
+
+    // QUEEN MOVE TESTS
+    RUN_TEST(shouldYield7MovesOnOneDiagonal);
+    RUN_TEST(shouldYieldNoMoves);
+    RUN_TEST(shouldYield2MovesOnEachDiagonalOpponentStop);
+    RUN_TEST(shouldYield2MovesOnEachDiagonalOwnPawnStop);
+    RUN_TEST(shouldYield2MovesOnEachDiagonalOpponentQueenStop);
+    RUN_TEST(shouldYield2MovesOnEachDiagonalOwnQueenStop);
+
+    // PLAYER MOVE TESTS
+    RUN_TEST(shouldYield2WhitePlayerPawnsWith2MovesEach);
+
+    // GAME ENGINE TESTS
+    RUN_TEST(shouldReturn2Positions);
+
+
     UNITY_END();
 }
