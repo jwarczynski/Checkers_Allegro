f50f524719349cd375777f2954757f8a2895565b
Jędrzej Warczyński <jedrzej.warczynski@student.put.poznan.pl>
2023-07-24 00:09:50 +0200

    refactor pawn capture move tests

--- Test/include/pawnCaptureTest.h
+++ Test/include/pawnCaptureTest.h
@@ -2,11 +2,11 @@
 // Created by Jj on 16/07/2023.
 //
 
-#ifndef CHECKERS_PAWNCAPTURETEST_H
-#define CHECKERS_PAWNCAPTURETEST_H
+#ifndef CHECKERS_PAWN_CAPTURE_TEST_H
+#define CHECKERS_PAWN_CAPTURE_TEST_H
 
 void shouldYield2PawnCaptureMovesWith2CapturesEach();
 void shouldYield4PawnCaptureMovesWith2CapturesEach();
 
 
-#endif //CHECKERS_PAWNCAPTURETEST_H
+#endif //CHECKERS_PAWN_CAPTURE_TEST_H
