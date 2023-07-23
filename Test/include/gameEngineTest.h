b8115e27073db380681e5cb66334675569e1673b
Jędrzej Warczyński <jedrzej.warczynski@student.put.poznan.pl>
2023-07-24 00:11:51 +0200

    add parsing console input test

---
+++ Test/include/gameEngineTest.h
@@ -0,0 +1,10 @@
+//
+// Created by Jj on 22/07/2023.
+//
+
+#ifndef CHECKERS_GAME_ENGINE_TEST_H
+#define CHECKERS_GAME_ENGINE_TEST_H
+
+void shouldReturn2Positions();
+
+#endif //CHECKERS_GAME_ENGINE_TEST_H
---
+++ Test/src/gameEngineTest.c
@@ -0,0 +1,23 @@
+//
+// Created by Jj on 22/07/2023.
+//
+
+#include "../include/gameEngineTest.h"
+#include "gameEngine/gameEngine.h"
+#include "../Unity/src/unity.h"
+#include "gameEngine/playerMove.h"
+#include "input/consoleInputParser.h"
+
+void shouldReturn2Positions() {
+    initPlayerMoves();
+    int count;
+    Position *path = parseUserInput("A1 B2 #", &count);
+
+    TEST_ASSERT_EQUAL_INT(2, count);
+
+    TEST_ASSERT_EQUAL_INT(0, path[0].row);
+    TEST_ASSERT_EQUAL_INT(0, path[0].col);
+
+    TEST_ASSERT_EQUAL_INT(1, path[1].row);
+    TEST_ASSERT_EQUAL_INT(1, path[1].col);
+}
