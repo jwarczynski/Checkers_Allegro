b2f46490305bf83b90891820e99f904548ba0f52
Jędrzej Warczyński <jedrzej.warczynski@student.put.poznan.pl>
2023-07-28 22:55:40 +0200

    add drawing application window

--- Main/include/ui/display/boardPainter.h
+++ Main/include/ui/display/boardPainter.h
@@ -6,14 +6,25 @@
 #define CHECKERS_BOARD_PAINTER_H
 
 #include "allegro5/color.h"
+#include "allegro5/allegro_font.h"
 
 // INTERFACE
+void drawInterface();
 void drawBoard();
 
 // implementation
 void drawSquares();
 void drawRow(int row);
 void drawSquare(int row, int column, ALLEGRO_COLOR color);
+
+void drawCoordinates();
+void drawHorizontalCoordinates();
+void drawHorizontalCoordinate(int row);
+void drawVerticalCoordinates();
+void drawVerticalCoordinate(int col);
+void drawCenteredText(const char *text, ALLEGRO_FONT *font, float x, float y);
+
+void drawBorder();
 
 void drawPieces();
 void drawPiece(int row, int col);
@@ -26,11 +37,20 @@ void drawBlackPawn(int row, int col);
 void drawWhiteQueen(int row, int col);
 void drawBlackQueen(int row, int col);
 void drawPawn(int row, int col, ALLEGRO_COLOR color);
-void drawQueen(int row, int col, ALLEGRO_COLOR color);
+void drawQueen(int row, int col, ALLEGRO_COLOR pawnColor, ALLEGRO_COLOR queenColor);
 
 void drawPawnCircle(int row, int col, ALLEGRO_COLOR color);
 void drawQueenCircle(int row, int col, ALLEGRO_COLOR color);
 void drawFilledCircle(int row, int col, float radius, ALLEGRO_COLOR color);
+
+void drawMenu();
+
+void drawSaveGameButton();
+void drawLoadGameButton();
+void drawNewGameButton();
+void drawUndoButton();
+void drawMenuButton(float y, char *text);
+void drawButton(float x, float y, float width, float height, const char *text, ALLEGRO_FONT *font);
 
 void changeToDisplayCoordinates(int *row, int *col);
 
